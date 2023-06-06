7cfe23ddc01f34af5fc737187c3fe24007833715
aleWidomlanski <widomlanski@hotmail.com>
2023-06-06 14:54:25 -0300

    sin local amplify pero node modules arreglado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import AdminPage from '@/components/AdminPage';
 import { AdminProvider } from '@/context/AdminProvider';
 
-/* import Amplify from 'aws-amplify';
+ import Amplify from 'aws-amplify';
 import awsconfig from '../aws-exports';
-Amplify.configure(awsconfig) */
+Amplify.configure(awsconfig) 
 
 const HomePage = () => {
 	return (
