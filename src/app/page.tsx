5f0c1740bc089ae7fef0855a3db7350c3ba4a860
aleWidomlanski <widomlanski@hotmail.com>
2023-06-08 14:15:39 -0300

    s/ amplify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import AdminPage from '@/components/AdminPage';
 import { AdminProvider } from '@/context/AdminProvider';
 
-/* import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
-Amplify.configure(awsconfig) 
- */
+
 const HomePage = () => {
 	return (
 		<AdminProvider>
